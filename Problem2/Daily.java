926f060bdd06a166091b25f0b438d551be7a943b
SadnurIslam <114425532+SadnurIslam@users.noreply.github.com>
2022-10-29 22:42:48 +0600

    Add files via upload

---
+++ Problem2/Daily.java
@@ -0,0 +1,25 @@
+public class Daily extends Appointment
+
+{
+
+
+    public Daily(String description)
+
+    {
+
+
+        setDescription(description);
+
+    }
+
+
+    public boolean occursOn(int year, int month, int day)
+
+    {
+
+
+        return true;
+
+    }
+
+}
